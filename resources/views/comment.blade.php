7a6a86b957a137d46a34140692e63b8ece55405f
”ken3pei” <kensuke1202left@icloud.com>
2020-04-09 07:07:56 +0000

    2020/04/09

--- resources/views/comment.blade.php
+++ resources/views/comment.blade.php
@@ -4,13 +4,26 @@
 <div class="training-contents">
     <div id="comment_contents">
         <div class="container comment_container">
-            <div class="section">
+            <div class="section row">
                 <p>Content</p>
-                <form class="d-inline-block ml-4" action="{{ action('CommentController@create', ['id' => $training->id]) }}" method="post" enctype="multipart/form-data">
-                    <textarea type="text" row="4" name="body"></textarea>
-                    <input type="submit" value="コメント">
-                    @csrf
-                </form>
+                <!--<p>{{ $training->user->name }}</p>-->
+                <!--<p>{{ $training->body }}</p>-->
+                <div class="comment-area col-md-6">
+                    <div class="tr-user">
+                    <p>{{ $training->user->name }}</p>
+                    <p>{{ $training->body }}</p>
+                    </div>
+                    <form action="{{ action('CommentController@create', ['id' => $training->id]) }}" method="post" enctype="multipart/form-data">
+                        <textarea type="text" row="4" name="body"></textarea>
+                        <input type="submit" value="コメント">
+                        @csrf
+                    </form>
+                </div>
+                <div class="col-md-6">
+                    <div>
+                        <!--コメントの一覧を表示-->
+                    </div>
+                </div>
             </div>
         </div>
     </div>

e8734aa167d1f560fa7baa780426488f19ad0b23
ken3pei <kensuke1202left@icloud.com>
2020-05-22 02:37:58 +0000

    add check icon

---
+++ resources/js/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+{
+    // function update() {
+        // document.getElementById('user_name').textContent = "Change!";
+        // document.getElementById('button').addEventListener('click', () => {
+        //     const inp = document.getElementById('user_name').message;
+        //     console.log(inp);
+        // });
+        
+        // document.querySelectorAll('p').textContent = "Change!";
+    // }
+    
+    // setTimeout(update, 1000);
+}
